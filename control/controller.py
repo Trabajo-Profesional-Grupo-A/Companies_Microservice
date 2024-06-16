8205189c6a87bbaa99197df61f4d2c7967fa0148
Martin <mscazzola@fi.uba.ar>
2024-06-16 18:01:06 -0300

    .

--- control/controller.py
+++ control/controller.py
@@ -1,8 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from control.routers import company
-from annoy import AnnoyIndex
-from scipy.spatial.distance import cosine
 
 app = FastAPI(
     title="Companies API", description="This is the API for the companies service."
