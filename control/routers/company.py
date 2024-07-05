ec413fb21c51b8cc420ebdf01ca20571126c494d
Martin <mscazzola@fi.uba.ar>
2024-07-05 20:46:11 -0300

    fix deploy

--- control/routers/company.py
+++ control/routers/company.py
@@ -14,12 +14,9 @@ from control.codes import (
     BAD_REQUEST,
     CONFLICT,
 )
-import firebase_admin
-import os
 
 from control.models.models import CompanySignUp, CompanySignIn, CompanyResponse
 from auth.auth_handler import hash_password, check_password, generate_token, decode_token
-from control.routers.aux import get_user_from_email
 
 router = APIRouter(
     tags=["companies"],
@@ -28,11 +25,6 @@ router = APIRouter(
 origins = ["*"]
 
 from repository.company_repository import create_company, get_company
-
-from firebase_admin import credentials, storage
-
-cred = credentials.Certificate("firebase_credentials.json")
-firebase_admin.initialize_app(cred, {"storageBucket": "tpp-grupoa.appspot.com"})
 
 @router.post("/sign-up")
 def sign_up(company: CompanySignUp):
@@ -75,39 +67,6 @@ def get_company_by_token(token: str):
         if not company:
             raise HTTPException(status_code=COMPANY_NOT_FOUND, detail="Company not found.")
         return CompanySignUp.parse_obj(company)
-
-    except ValueError as e:
-        raise HTTPException(status_code=BAD_REQUEST, detail=str(e))
-    
-@router.post("/upload-image")
-def upload_image():
-    """
-    Upload an image.
-    """
-    try:
-        script_dir = os.path.dirname(__file__)
-        image_path = os.path.join(script_dir, "../../image.png")
-        
-        if not os.path.exists(image_path):
-            raise FileNotFoundError(f"No such file or directory: '{image_path}'")
-        
-        bucket = storage.bucket() 
-        blob = bucket.blob("image.png")
-        blob.upload_from_filename(image_path)
-        return {"message": "Image uploaded successfully."}
-    except Exception as e:
-        raise HTTPException(status_code=BAD_REQUEST, detail=str(e))
-
-@router.get("/prueba/conexion")    
-def api_prueba_conexion(email: str):
-    """
-    Get a user by email.
-    """
-    try:
-        user = get_user_from_email(email)
-        if not user:
-            raise HTTPException(status_code=USER_NOT_FOUND, detail="User not found.")
-        return {"message": "User found successfully.", "user": user}
 
     except ValueError as e:
         raise HTTPException(status_code=BAD_REQUEST, detail=str(e))
